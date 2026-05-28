99d9d5a0525324850fdf2dda5de0617a902d8fc6
CuberVR <117398907+cosmic4445@users.noreply.github.com>
2026-05-28 16:13:46 -0400

    Update login.js

--- login.js
+++ login.js
@@ -1,5 +1,23 @@
 const users = [
    { username: "Cuber432", password: "Yerp214@2!" },
+   { username: "Androm2", password: "Yerp@235" },
+   { username: "Tophat", password: "Yerp@#44" },
+   { username: "Saturn", password: "Yerp@442 " },
+   { username: "Destroyer", password: "Yerp@240 " },
+   { username: "CSVR", password: "Yerp@845" },
+   { username: "Dryice", password: "Yerp@!29" },
+   { username: "Hoppaa", password: "Yerp@77#" },
+   { username: "Lazerr", password: "Yerp@856 " },
+   { username: "Meshew", password: "Yerp@866" },
+   { username: "Semi", password: "Yerp@88%" },
+   { username: "Soul", password: "Yerp@2@3" },
+   { username: "Wraxo", password: "Yerp@%64" },
+   { username: "Wisefish", password: "Yerp@0*#" },
+   { username: "Breezxy", password: "Yerp@688" },
+   { username: "Sk1lld", password: "Yerp@55@" },
+   { username: "Suitte", password: "Yerp@2@*" },
+   { username: "Xboom", password: "Yerp@25*" },
+   { username: "Skippy", password: "Yerp@&57 " },
 ];
 function getUsers() {
   const stored = localStorage.getItem("devAccounts");
